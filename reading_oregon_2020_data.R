51ead95073cdc7521438c89acf3cdc5282179011
ctadjiki <ctadjiki@uoregon.edu>
2022-01-25 17:13:26 -0800

    Adding Oreogn 2020 Data

---
+++ reading_oregon_2020_data.R
@@ -0,0 +1,97 @@
+
+library(pacman)
+p_load(tidyverse, rvest, lubridate, janitor, 
+       data.table, readr, readxl, dplyr, skimr, broom)
+
+
+# OFFENSEloading data
+incident_df = read.csv("NIBRS_incident.csv")
+offense_df = read.csv("NIBRS_OFFENSE.csv")
+offense_type_df = read.csv("NIBRS_OFFENSE_TYPE.csv")
+relationship_df = read.csv("NIBRS_RELATIONSHIP.csv")
+victim_df = read.csv("NIBRS_VICTIM.csv")
+offender_df = read.csv("NIBRS_OFFENDER.csv")
+victim_type_df = read.csv("NIBRS_VICTIM_TYPE.csv")
+victim_circumstances_df = read.csv("NIBRS_VICTIM_CIRCUMSTANCES.csv")
+victim_offender_rel_df = read.csv("NIBRS_VICTIM_OFFENDER_REL.csv")
+# _df = read.csv("NIBRS_.csv")
+# _df = read.csv("NIBRS_.csv")
+
+joined_df = left_join(incident_df, offense_df, by = "INCIDENT_ID")
+
+# amount of crime(types of crime), num_gp_per_house, income, school open or not
+# 2020-1
+# 2020-2
+
+
+# NIBRS_ACTIVITY_TYPE.csv
+# NIBRS_AGE.csv
+# NIBRS_ARRESTEE.csv
+# NIBRS_ARRESTEE_WEAPON.csv
+# NIBRS_ARREST_TYPE.csv
+# NIBRS_ASSIGNMENT_TYPE.csv
+# NIBRS_BIAS_LIST.csv
+# NIBRS_BIAS_MOTIVATION.csv
+# NIBRS_CIRCUMSTANCES.csv
+# NIBRS_CLEARED_EXCEPT.csv
+# NIBRS_CRIMINAL_ACT.csv
+# NIBRS_CRIMINAL_ACT_TYPE.csv
+# NIBRS_DRUG_MEASURE_TYPE.csv
+# NIBRS_ETHNICITY.csv
+# NIBRS_INJURY.csv
+# NIBRS_JUSTIFIABLE_FORCE.csv
+# NIBRS_LOCATION_TYPE.csv
+# NIBRS_OFFENDER.csv
+# NIBRS_OFFENSE.csv
+# NIBRS_OFFENSE_TYPE.csv
+# NIBRS_PROPERTY.csv
+# NIBRS_PROPERTY_DESC.csv
+# NIBRS_PROP_DESC_TYPE.csv
+# NIBRS_PROP_LOSS_TYPE.csv
+# NIBRS_RELATIONSHIP.csv
+# NIBRS_SUSPECTED_DRUG.csv
+# NIBRS_SUSPECTED_DRUG_TYPE.csv
+# NIBRS_SUSPECT_USING.csv
+# NIBRS_USING_LIST.csv
+# NIBRS_VICTIM.csv
+# NIBRS_VICTIM_CIRCUMSTANCES.csv
+# NIBRS_VICTIM_INJURY.csv
+# NIBRS_VICTIM_OFFENDER_REL.csv
+# NIBRS_VICTIM_OFFENSE.csv
+# NIBRS_VICTIM_TYPE.csv
+# NIBRS_WEAPON.csv
+# NIBRS_WEAPON_TYPE.csv
+# NIBRS_incident.csv
+# NIBRS_month.csv
+# REF_RACE.csv
+# REF_STATE.csv
+# agencies.csv
+
+
+
+
+
+incident_names = names(incident_df)
+offense_names = names(offense_df)
+offense_type_names = names(offense_type_df)
+relationship_names = names(relationship_df)
+victim_names = names(victim_df)
+victim_circumstances_names = names(victim_circumstances_df)
+
+
+
+
+offense_names = c(offense_names, rep(NA, 15 - length(offense_names)))
+offense_type_names = c(offense_type_names, rep(NA, 15 - length(offense_type_names)))
+
+col_names = data.frame(incident_names,offense_names,offense_type_names)
+length(unique(offense_type_df$OFFENSE_TYPE_ID))
+length(unique(offense_type_df$OFFENSE_CATEGORY_NAME))
+length(unique(offense_type_df$OFFENSE_NAME))
+length(unique(offense_type_df$OFFENSE_CODE))
+
+
+
+
+
+
